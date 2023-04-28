6e94a54fc5eaaf687fe31fcb90171720297babcd
Agos <cuentasecundariaagosss@gmail.com>
2023-04-28 16:53:05 -0300

    trc

--- src/screens/Contenido.jsx
+++ src/screens/Contenido.jsx
@@ -3,7 +3,7 @@ import im1 from '../assets/ml1.png'
 import im2 from '../assets/ml2.png'
 import im3 from '../assets/ml3.png'
 import im4 from '../assets/ml4.png'
-
+import im5 from '../assets/ml5.png'
 const Contenido = () => {
   const [lateralMenu,setLateralMenu] = useState(false);
 
@@ -177,14 +177,183 @@ const Contenido = () => {
             <br></br>
             #crea una matriz con el valor 8 en todas sus posiciones de dimension 2x2
           </code>
-          
+          <h3>Redimension de matrices</h3>
+          <p></p>
           <h2>Pandas</h2>
           <p>HS</p>
           <h2>Scikit Learn</h2>
           <p>
-            Scikit-learn es una biblioteca de aprendizaje automático para Python que proporciona una amplia gama de algoritmos y herramientas para tareas de minería y análisis de datos. Con Scikit-learn, los usuarios pueden aplicar técnicas de aprendizaje automático como clasificación, regresión, agrupamiento y reducción de dimensionalidad a sus datos de manera eficiente y fácil de usar. Scikit-learn es muy popular en la comunidad de Python debido a su facilidad de uso, documentación detallada y la gran cantidad de ejemplos y recursos disponibles.
+            Scikit-learn es una biblioteca de aprendizaje automático para Python que proporciona una amplia gama de algoritmos {`(supervisados y no supervisados)`} y herramientas para tareas de minería y análisis de datos. Con Scikit-learn, los usuarios pueden aplicar técnicas de aprendizaje automático como clasificación, regresión, agrupamiento y reducción de dimensionalidad a sus datos de manera eficiente y fácil de usar. Scikit-learn es muy popular en la comunidad de Python debido a su facilidad de uso, documentación detallada y la gran cantidad de ejemplos y recursos disponibles.
           </p>
-          <h2>Machine Learning</h2>
+          <h3>DataSets</h3>
+          <p>
+            En scikit-learn, un dataset es un conjunto de datos estructurado que se utiliza para entrenar y evaluar modelos de aprendizaje automático. Los datasets en scikit-learn se componen de dos partes: un conjunto de características (features) y un conjunto de etiquetas (labels), que se utilizan para entrenar un modelo predictivo.
+            <br></br>
+            Por ejemplo, en un problema de clasificación de imágenes, un dataset podría contener imágenes en escala de grises como características y las etiquetas correspondientes que indican las clases de las imágenes (por ejemplo, "perro" o "gato"). En un problema de regresión, las características podrían ser variables numéricas y las etiquetas podrían ser valores continuos.
+            <br></br>
+            Scikit-learn incluye varios datasets incorporados que se pueden cargar directamente desde el paquete, como el conjunto de datos Iris y el conjunto de datos de dígitos escritos a mano (MNIST). Estos datasets se utilizan comúnmente como ejemplos de cómo utilizar diferentes modelos de aprendizaje automático en scikit-learn. Además, scikit-learn también proporciona herramientas para cargar y trabajar con datasets personalizados.
+          </p>
+          <p><span className='marc'>Visualizar la informacion de un DataSet</span></p>
+          <code className='coding'>
+           from sklearn import datasets
+           <br></br><br></br>
+           #importamos el dataset de los datos Iris
+           <br></br>
+           iris = dataset.load_iris()
+           <br></br><br></br>
+           #muestra el contenido de todo el dataset
+           <br></br>
+           print(iris)
+           <br></br><br></br>
+           #muestra una lista con los de cada dato dentro del dataset
+           <br></br>
+           print(iris.keys())
+           <br></br><br></br>
+           #muestra la descripcion detallada de los datos 
+           <br></br>
+           print(iris.DESCR)
+           <br></br><br></br>
+           #muestra la dimension de la data como (500 x 13), es decir 500 filas y 13 columnas
+           <br></br>
+           print(iris.data.shape)
+           <br></br><br></br>
+           #muestra la informacion de las columnas en forma de una lista con los nombres
+           <br></br>
+           print(iris.feature_names)
+          </code>
+          <p><span className='marc'>Preparacion de datos para aplicarlo en un modelo de machine learning</span></p>
+          <p>
+            Vamos a tener en cuenta el set de datos anteriores para realizar esto. 
+            <br></br>
+            Para extraer informacion de un dataset de SickitLearn hay que tener en claro que datos representaran los de la variable dependiente y los que representaran a la variable independiente. En estos sets los valores de la variable dependiente se encuentran en la seccion "DATA" y podemos seleccionar cualquiera de las columnas que nos llame la atencion, mientras que los valores de la variable dependiente se encuentran en la seccion de "TARGET" y es una lista unica para cualquier valor dependiente que  queramos analizar. 
+          </p>
+          <code className='coding'>
+            x = iris.data[:, np.newaxis, 5]
+            <br></br>
+            y = iris.target
+          </code>
+          <p>
+            "iris.data" se refiere a los datos del conjunto de datos Iris. Este conjunto de datos contiene información sobre diferentes características de las flores iris, como el ancho y largo de los pétalos y sépalos.<br></br>
+            La sintaxis "{`[ : , np.newaxis, 5 ]`}" se usa para seleccionar una columna específica de los datos y agregar una nueva dimensión a esa columna. En este caso, la quinta columna {`(índice 5)`} se selecciona y se agrega una dimensión adicional a esa columna.
+            La nueva dimensión se agrega usando "np.newaxis", que es un alias para "None" en numpy. Básicamente, agrega una dimensión adicional a los datos seleccionados.<br></br>
+            El resultado final es que "x" contiene una matriz de datos que consiste en una sola columna {`(la quinta columna del conjunto de datos Iris)`} con una dimensión adicional agregada.
+          </p>
+          <h3>Regresion lineal simple</h3>
+          <p>Importar la libreria</p>
+          <code className='coding'>
+            from sklearn import linear_model            
+          </code>
+          <p><span className='marc'>Comandos basicos</span></p>
+          <code className='coding'>
+            #define el algoritmo
+            <br></br>
+            linear_model.LinearRegresion()
+          </code>
+          <code className='coding'>
+            #entrena el modelo
+            <br></br>
+            fit(x,y)
+          </code>
+          <code className='coding'>
+            #predecir modelo
+            <br></br>
+            predict(x)
+          </code>
+          <code className='coding'>
+            #conocer pendiente de la funcion
+            <br></br>
+            _coef
+          </code>
+          <code className='coding'>
+            #conocer interseccion
+            <br></br>
+            intercept_
+          </code>
+          <p><span className='marc'>Ejemplo de regresion lineal</span></p>
+          <code className='coding'>
+            Import numpy as np
+            <br></br>
+            from sklearn import linear_model
+            <br></br>
+            Import matplotlib.pyplot as plt
+            <br></br><br></br>
+            # Datos de ejemplo
+            <br></br>
+            eje_x = np.array([1, 2, 3, 4, 5, 6])
+            <br></br>
+            eje_y = np.array([20, 30, 44, 67, 90, 103])
+            <br></br><br></br>
+            # Crear el modelo de regresión lineal
+            <br></br>
+            regresion = LinearRegression()
+            <br></br><br></br>
+            # Ajustar el modelo a los datos
+            <br></br>
+            regresion.fit(eje_x.reshape(-1, 1), eje_y)
+            <br></br><br></br>
+            # Predecir los valores para los datos de entrada
+            <br></br>
+            predicciones = regresion.predict(eje_x.reshape(-1, 1))
+            <br></br><br></br>
+            # Visualizar los datos y la línea de regresión
+            <br></br>
+            plt.scatter(eje_x, eje_y)
+            <br></br>
+            plt.plot(eje_x, predicciones, color='red')
+            <br></br>
+            plt.xlabel('Eje X')
+            <br></br>
+            plt.ylabel('Eje Y')
+            <br></br>
+            plt.title('Regresión lineal simple')
+            <br></br>
+            plt.show()
+            <br></br><br></br>
+            #ahora vamos a ver las caracteristicas 
+          </code>
+          <p>Ejemplo de regresion lineal utilizando un dataset de sickit learn</p>
+          <code className='coding'>
+            import numpy as np
+            <br></br>
+            from sklearn.datasets import load_diabetes
+            <br></br>
+            from sklearn.linear_model import LinearRegression
+            <br></br>
+            import matplotlib.pyplot as plt
+            <br></br><br></br>
+            # Cargar el dataset diabetes
+            diabetes = load_diabetes()
+            <br></br><br></br>
+            # Seleccionar la característica "age" como variable independiente (X)
+            X = diabetes.data[:, np.newaxis, 2]
+            <br></br><br></br>
+            # Seleccionar la variable dependiente (y)
+            y = diabetes.target
+            <br></br><br></br>
+            # Crear un modelo de regresión lineal
+            reg = LinearRegression()
+            <br></br><br></br>
+            # Entrenar el modelo con los datos de edad
+            reg.fit(X, y)
+            <br></br>
+            # Predecir los valores de la variable dependiente (y) con el modelo entrenado
+            y_pred = reg.predict(X)
+            <br></br>
+            # Graficar los datos y la línea de regresión
+            <br></br>
+            plt.scatter(X, y, color='blue')
+            <br></br>
+            plt.plot(X, y_pred, color='red', linewidth=3)
+            <br></br>
+            plt.title('Regresión lineal de edad y diabetes')
+            <br></br>
+            plt.xlabel('Edad')
+            <br></br>
+            plt.ylabel('Nivel de glucosa en sangre')
+            <br></br>
+            plt.show()
+          </code>
+          <img src={im5} alt="5" className='img'/>
         </div>
       </div>
     </main>
